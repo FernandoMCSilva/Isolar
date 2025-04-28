608f37c79fbd58c3729f209ad66edf5ff59ebaea
Gilemar007 <gileadesantos2004@gmail.com>
2025-04-28 18:51:13 -0300

    Ajustes

--- options_module.py
+++ options_module.py
@@ -2,7 +2,7 @@ from selenium.webdriver.chrome.options import Options
 
 def create_options():
     options = Options()
-    # options.add_argument('--headless')
+    options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     return options
