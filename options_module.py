132a20e6060bc063ef7d2a610176bad019253303
Fernando Silva <107194852+FernandoMCSilva@users.noreply.github.com>
2025-06-06 15:56:40 -0300

    Update options_module.py

--- options_module.py
+++ options_module.py
@@ -2,7 +2,7 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 def create_options():
-    binary = FirefoxBinary("/usr/bin/firefox")  # Força uso do caminho correto
+    binary = FirefoxBinary("/usr/bin/firefox")
     options = Options()
     options.binary = binary
     options.add_argument('--headless')  # Comente se quiser com GUI
