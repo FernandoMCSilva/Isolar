3cecbd9eff144cfea3ae06ca4885f1247a7dcfbe
Fernando Silva <107194852+FernandoMCSilva@users.noreply.github.com>
2025-06-06 12:08:46 -0300

    Update options_module.py

--- options_module.py
+++ options_module.py
@@ -2,7 +2,8 @@ from selenium.webdriver.chrome.options import Options
 
 def create_options():
     options = Options()
-    # options.add_argument('--headless')
+    options.add_argument('--headless=new')  # novo modo headless
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--window-size=1920,1080')  # importante para renderizar menus
     return options
